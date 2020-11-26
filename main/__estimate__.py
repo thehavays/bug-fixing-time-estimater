487ee245ee0b2a40e62cb13c9a9a00181cf81534
erayh <eray.havaylar@vestel.com.tr>
2020-11-26 23:36:04 +0300

    four and five strategy implemented

--- main/__estimate__.py
+++ main/__estimate__.py
@@ -16,6 +16,7 @@ class Issue:
     issue: tuple = None
     id: str = None
     summary: str = None
+    assignee: str = None
     type: str = None
     priority: str = None
     words: [] = None
@@ -23,6 +24,7 @@ class Issue:
     def __post_init__(self):
         self.id: str = str(issue[0])
         self.summary: str = str(issue[1])
+        self.assignee: str = str(issue[11])
         self.type: str = str(issue[9])
         self.priority: str = str(issue[10])
         self.words: [] = self.summary.split()
@@ -39,39 +41,58 @@ def get_fixed_time(id):
     return postgresAdapter.get_issue_fixed_time(id)
 
 
-def get_most_common(issue):
+def get_most_common(issue, is_cluster, is_assignee):
     big_array = []
     for word in issue.words:
-        json_array_result = elasticsearchAdapter.search_by_summary(word)
-        big_array.extend(get_result_ids(json_array_result))
+        json_array_result = elasticsearchAdapter.search_by_summary(word, is_cluster, is_assignee, issue)
+        temp = get_result_ids(json_array_result)
+        big_array.extend(temp)
     counter = Counter(big_array)
     return counter.most_common(10)
 
 
-def estimate_strategy_one(issue):
-    counter_array = get_most_common(issue)
+def get_arithmetic_mean(most_common):
     total_second = 0
     count = 0
-    for element in counter_array:
+    for element in most_common:
         time = get_fixed_time(element[0])
         if time != 0:
             count = count + 1
-            total_second += get_fixed_time(element[0])
+            total_second += time
     return total_second / count
 
 
-def estimate_strategy_two(issue):
-    counter_array = get_most_common(issue)
+def get_weighted_mean(most_common):
     total_second = 0
     count = 0
-    for element in counter_array:
+    for element in most_common:
         element_id = element[0]
         element_count = element[1]
         time = get_fixed_time(element_id)
         if time != 0:
             count = count + element_count
-            total_second += get_fixed_time(element_id) * element_count
+            total_second += time * element_count
     return total_second / count
+
+
+def estimate_strategy_one(most_common):
+    return get_arithmetic_mean(most_common)
+
+
+def estimate_strategy_two(most_common):
+    return get_weighted_mean(most_common)
+
+
+def estimate_strategy_three(most_common):
+    return get_weighted_mean(most_common)
+
+
+def estimate_strategy_four(most_common):
+    return get_arithmetic_mean(most_common)
+
+
+def estimate_strategy_five(most_common):
+    return get_weighted_mean(most_common)
 
 
 actual_fixed_times = []
@@ -79,6 +100,7 @@ estimate_strategy_one_fixed_times = []
 estimate_strategy_two_fixed_times = []
 estimate_strategy_three_fixed_times = []
 estimate_strategy_four_fixed_times = []
+estimate_strategy_five_fixed_times = []
 
 for x in range(3):
     actual_fixed_times.clear()
@@ -86,15 +108,33 @@ for x in range(3):
     estimate_strategy_two_fixed_times.clear()
     estimate_strategy_three_fixed_times.clear()
     estimate_strategy_four_fixed_times.clear()
+    estimate_strategy_five_fixed_times.clear()
 
     test = postgresAdapter.get_issues_by_rate((x + 1) * 10)
 
     for issue in test:
         var = Issue(issue)
-        actual_fixed_times.append(get_fixed_time(var.id))
-        estimate_strategy_one_fixed_times.append(estimate_strategy_one(var))
-        estimate_strategy_two_fixed_times.append(estimate_strategy_two(var))
-        # estimate_strategy_three_fixed_times.append(estimate_strategy_three(var))
-        # estimate_strategy_four_fixed_times.append(estimate_strategy_four(var))
+        most_common_clustered = get_most_common(var, is_cluster=True, is_assignee=False)
+        most_common = get_most_common(var, is_cluster=False, is_assignee=False)
+        most_common_assignee = get_most_common(var, is_cluster=True, is_assignee=True)
 
-    print(len(actual_fixed_times))
+        fixed_time = get_fixed_time(var.id)
+        strategy_one_fixed_time = estimate_strategy_one(most_common)
+        strategy_two_fixed_time = estimate_strategy_two(most_common)
+        strategy_three_fixed_time = estimate_strategy_three(most_common_clustered)
+        strategy_four_fixed_time = estimate_strategy_four(most_common_assignee)
+        strategy_five_fixed_time = estimate_strategy_five(most_common_assignee)
+        result_json = {"id": var.id, "fixed_time": fixed_time,
+                       "one": strategy_one_fixed_time,
+                       "two": strategy_two_fixed_time,
+                       "three": strategy_three_fixed_time,
+                       "four": strategy_four_fixed_time,
+                       "five": strategy_five_fixed_time}
+        print(result_json)
+
+        actual_fixed_times.append(fixed_time)
+        estimate_strategy_one_fixed_times.append(strategy_one_fixed_time)
+        estimate_strategy_two_fixed_times.append(strategy_two_fixed_time)
+        estimate_strategy_three_fixed_times.append(strategy_three_fixed_time)
+        estimate_strategy_four_fixed_times.append(strategy_four_fixed_time)
+        estimate_strategy_five_fixed_times.append(strategy_five_fixed_time)
